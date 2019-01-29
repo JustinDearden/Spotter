45a0be109e9220384f75ff86a3636df42e8dcb19
Justin Dearden <deardenj@protonmail.com>
2019-01-29 00:41:32 -0500

    Styled Register Page

--- register.php
+++ register.php
@@ -19,8 +19,30 @@
 <head>
 	<title>Welcome to Spotter!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="assets/js/register.js"></script>
 </head>
 <body>
+
+	<?php
+	if(isset($_POST['registerButton'])) {
+		echo '	<script>
+		$(document).ready(function() {
+			$("#loginForm").hide();
+			$("#registerForm").show();
+		});
+		</script>';
+	} else {
+		echo '	<script>
+		$(document).ready(function() {
+			$("#loginForm").show();
+			$("#registerForm").hide();
+		});
+		</script>';
+	}
+	?>
+
 	<div id="background">
 
 		<div id="loginContainer">
@@ -39,6 +61,10 @@
 					</p>
 
 					<button type="submit" name="loginButton">LOG IN</button>
+
+					<div class="hasAccountText">
+						<span id="hideLogin">Don't have an account yet? Signup here.</span>
+					</div>
 					
 				</form>
 
@@ -92,11 +118,26 @@
 					</p>
 
 					<button type="submit" name="registerButton">SIGN UP</button>
+
+					<div class="hasAccountText">
+						<span id="hideRegister">Already have an account? Log in here.</span>
+					</div>
 					
 				</form>
 
-
 			</div>
+
+			<div id="loginText">
+				<h1>Get great music, right now</h1>
+				<h2>Listen to loads of songs for free</h2>
+				<ul>
+					<li>Discover music you'll fall in love with</li>
+					<li>Create your own playlists</li>
+					<li>Follow artists to keep up to date</li>
+				</ul>	
+			</div>
+
+
 		</div>
 	</div>
 
